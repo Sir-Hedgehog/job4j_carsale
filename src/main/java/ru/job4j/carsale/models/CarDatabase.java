f440a47fe0bec64ad42a7975aa097f758d97f198
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2020-09-26 16:26:19 +0300

    Change of database properties

--- src/main/java/ru/job4j/carsale/models/CarDatabase.java
+++ src/main/java/ru/job4j/carsale/models/CarDatabase.java
@@ -4,8 +4,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
